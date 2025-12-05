04f5d65fde4296eea46fe3648b05c0fb0394b995
Naveen S V <naveensv77@gmail.com>
2025-12-05 17:30:02 +0530

    Pushing browser page

--- playwright/browser.js
+++ playwright/browser.js
@@ -1,7 +1,7 @@
 import { chromium } from "playwright";
 
 const launchbrowser = async () => {
-  return await chromium.launch({ headless: false });
+  return await chromium.launch({ headless: true });
 };
 
 export default launchbrowser;
